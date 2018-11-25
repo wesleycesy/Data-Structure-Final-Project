db096d4a56aa0d2fd25193f1ef3b17c980a83a65
Wesley Campos da Silva <45320141+wesleycesy@users.noreply.github.com>
2018-11-25 16:04:06 -0200

    Update AVL.c

--- AVL.c
+++ AVL.c
@@ -1,146 +1,161 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct Tno{
-	int key;
-	int alt;
-	struct Tno *left;
-	struct Tno *right;
-}no;
+typedef struct Node{
+	unsigned long key;
+	int height;
+	struct Node *left;
+	struct Node *right;
+	struct Node *avl;
+}node;
 
-void print_tree(no *first)
+int flag = 0;
+
+void print_tree(node *first)
 {
 	if(first != NULL)
 	{
 		print_tree(first->left);
-		printf("%d (%d) - ", first->key,first->alt);
+		printf("%lu (%d) - ", first->key,first->height);
 		print_tree(first->right);		
 	} 
 
 }
 
+node *search(node *first, unsigned long int x)
+{
+        if(first == NULL)
+        {
+            return NULL;
+        }
+        if((first)->key == x)
+        {
+            return first->avl;
+        }
+        if((first)->key < x) return search((first)->right,x);
+        else return search((first)->left,x);
+}
 
-
-void rot_right(no **first)
+void rot_right(node **first)
 {
 	if(*first == NULL) return;
 	
-	//(*first)->alt = 0;
-	
-	no *aux = (*first)->left;
+	node *aux = (*first)->left;
 	(*first)->left = aux->right;
 	aux->right = *first;
 	*first = aux;
-	
-	
-	//(*first)->alt+=1;
 }
 
-void rot_left(no **first)
+void rot_left(node **first)
 {
 	if(*first == NULL) return;
 	
-	//(*first)->alt = 0;
-	
-	no *aux = (*first)->right;
+	node *aux = (*first)->right;
 	(*first)->right = aux->left;
-	aux->left = *first;
-
-	
+	aux->left = *first;	
 	*first = aux;
-	
-	//(*first)->alt -= 1;
 }
 
-int insert_node(no **first, int key)
+int insert_node(node **first, unsigned long key, unsigned long not_key)
 {
 	if(*first == NULL)
 	{
-		*first = (no *) malloc(sizeof(no));
+		*first = (node *) malloc(sizeof(node));
 		if(first == NULL) return 0;
 		
 		(*first)->right = NULL;
 		(*first)->left = NULL;
-		(*first)->alt = 0;
-		(*first)->key = key;
+		(*first)->avl = NULL;
+		(*first)->height = 0;
+		//(*first)->key = key;
+		(*first)->key = not_key;
+
+		if(flag != 1) insert_node(&(*first), key, not_key);
 		return 1;
 	}
 	else
-		if(key == (*first)->key) return 0;
-		else if(key < (*first)->key)
+	if(not_key == (*first)->key)
+	{
+		flag = 1;
+		insert_node(&(*first)->avl, not_key, key);
+		//printf("key: %lu | fb: %d.\n\n",(*first)->key,(*first)->height);
+		flag = 0;
+		return 0;
+	}
+	else if(not_key < (*first)->key)
+	{
+		int a = insert_node(&(*first)->left, key, not_key);
+		//printf("no: %d | fb: %d | fb¹: %d\n\n",(*first)->key,(*first)->height,a);
+		if(a == 1)
 		{
-			int a = insert_node(&(*first)->left, key);
-			//printf("no: %d | fb: %d | fb¹: %d\n\n",(*first)->key,(*first)->alt,a);
-			if(a == 1)
-			{
-			(*first)->alt -= a;
-			if((*first)->alt == -1) return 1;
-			if((*first)->alt == 0) return 0;
+			(*first)->height -= a;
+			if((*first)->height == -1) return 1;
+			if((*first)->height == 0) return 0;
 			else
 			{
-				if((*first)->alt < 1)
+				if((*first)->height < 1)
 				{
-					if((*first)->alt * (*first)->left->alt < 0)
+					if((*first)->height * (*first)->left->height < 0)
 					{
 						rot_left(&(*first)->left);
 						rot_right(first);
-						if((*first)->alt == -1)
+						if((*first)->height == -1)
 						{
-							(*first)->left->alt = 0;
-							(*first)->left->alt = 1;
+							(*first)->left->height = 0;
+							(*first)->left->height = 1;
 						}
 						else
 						{
-							(*first)->right->alt = 0;
-							(*first)->left->alt = -(*first)->alt;
+							(*first)->right->height = 0;
+							(*first)->left->height = -(*first)->height;
                         			}
-                        			(*first)->alt = 0;
+                        			(*first)->height = 0;
 					}
 					else
 					{
 						rot_right(first);
-						(*first)->right->alt = 0;
-						(*first)->alt = 0;
+						(*first)->right->height = 0;
+						(*first)->height = 0;
 					}
 					return 0;
 				}
 			}
 		}else return 0;
-		}
-		else
-		{		
-			int b = insert_node(&(*first)->right, key);
-			//printf("no: %d | fb: %d | fb¹: %d\n\n",(*first)->key,(*first)->alt,b);			
-			if(b == 1)
-			{
-			(*first)->alt += b;
-			if((*first)->alt == 1) return 1;
-			if((*first)->alt == 0) return 0;
+	}
+	else
+	{		
+		int b = insert_node(&(*first)->right, key, not_key);
+		//printf("no: %d | fb: %d | fb¹: %d\n\n",(*first)->key,(*first)->height,b);			
+		if(b == 1)
+		{
+			(*first)->height += b;
+			if((*first)->height == 1) return 1;
+			if((*first)->height == 0) return 0;
 			else
 			{
-				if((*first)->alt > 1)
+				if((*first)->height > 1)
 				{	
-					if((*first)->alt * (*first)->right->alt < 0)
+					if((*first)->height * (*first)->right->height < 0)
 					{
 						rot_right(&(*first)->right);
 						rot_left(first);
-						if((*first)->alt == -1)
+						if((*first)->height == -1)
 						{
-							(*first)->left->alt = 0;
-							(*first)->right->alt = 1;
+							(*first)->left->height = 0;
+							(*first)->right->height = 1;
 						}
 						else
 						{
-							(*first)->right->alt = 0;
-							(*first)->left->alt = -(*first)->alt;
+							(*first)->right->height = 0;
+							(*first)->left->height = -(*first)->height;
 						}
-						(*first)->alt = 0;
+						(*first)->height = 0;
 					}
 					else
 					{
 						rot_left(first);
-						(*first)->left->alt = 0;
-						(*first)->alt = 0;
+						(*first)->left->height = 0;
+						(*first)->height = 0;
 					}
 					return 0;
 				}
@@ -150,43 +165,24 @@ int insert_node(no **first, int key)
 }
 void main()
 {
-	no *first = NULL;
+	node *first = NULL, *aux = NULL;
 	
-	insert_node(&first,50);
-	insert_node(&first,60);
-	insert_node(&first,40);
-	insert_node(&first,65);
-	insert_node(&first,55);
-	insert_node(&first,70);
-	insert_node(&first,63);
-	insert_node(&first,62);
-	insert_node(&first,1);
-	insert_node(&first,4);
-	insert_node(&first,56);
-	insert_node(&first,51);
-	insert_node(&first,57);
-	print_tree(first);
+	insert_node(&first,50,1);
+	insert_node(&first,60,2);
+	insert_node(&first,40,3);
+	insert_node(&first,65,4);
+	insert_node(&first,55,4);
+	insert_node(&first,54,4);
+	insert_node(&first,63,6);
+	insert_node(&first,62,7);
+	insert_node(&first,1,8);
+	insert_node(&first,4,9);
+	insert_node(&first,56,11);
+	insert_node(&first,51,11);
+	insert_node(&first,57,11);
 	
-	/*insert_node(&first,-4);
-	insert_node(&first,-5);
-	insert_node(&first,6);
-	insert_node(&first,5);
+	//print_tree(first);
 	
-	printf("%d\n",first->left->left->left->alt);
-	printf("%d\n",first->left->left->alt);
-	printf("%d\n",first->left->alt);
-	printf("%d\n",first->alt);
-	printf("%d\n",first->right->alt);
-	printf("%d\n",first->right->right->alt);
-	printf("%d\n",first->right->left->alt);
-	*/
-	/*
-	
-	insert_node(&first,-2);
-	insert_node(&first,10);
-	insert_node(&first,27);
-	insert_node(&first,7);
-	insert_node(&first,26);
-	insert_node(&first,8);
-	*/	
+	aux = search(first,4);
+	print_tree(aux);		
 }
