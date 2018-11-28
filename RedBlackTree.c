80738555dd0ffae3e95e209ef398478e41b61ad4
Roppwer <joaocastilho@alunos.utfpr.edu.br>
2018-11-28 20:31:33 -0200

    Create RedBlackTree.c
    Struct da redblack

---
+++ RedBlackTree.c
@@ -0,0 +1,19 @@
+//RED BLACK TREE
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Node{
+    char color; // 0 for black - 1 for black
+    unsigned long key;
+    unsigned long data;
+    struct Node *left, *right;
+}; struct Node Node;
+
+
+//Root is allways BLACK
+//If a node is red, then both its children are black.
+//Every path from a given node to any of its descendant NIL nodes contains the same number of black nodes.
+
+void main(){
+
+}
