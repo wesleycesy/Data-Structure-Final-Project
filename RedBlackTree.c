18386e9dcbcf36560cf7e774d6fc5977906b1451
Roppwer <joaocastilho@alunos.utfpr.edu.br>
2018-11-29 01:41:07 -0200

    Update RedBlackTree.c
    Melhorias.
    
    Aux: https://www.youtube.com/watch?v=IuG3WSaDumc&index=71&list=PLpPXw4zFa0uKKhaSz87IowJnOTzh9tiBk

--- RedBlackTree.c
+++ RedBlackTree.c
@@ -1,49 +1,112 @@
 //RED BLACK TREE
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 struct Node{
     char isBlack; // 0 for false (red) / 1 for true (Black)
+    char isLeftChild; //0 for false / 1 for true
     unsigned long key;
     unsigned long data;
     struct Node *left, *right, *parent ;
-}; struct Node Node;
+}; typedef struct Node Node;
 
 
-No *newNode(unsigned long key, unsigned long data){
-    No *temp = (Node *) malloc(sizeof(Node));
+Node *newNode(unsigned long key, unsigned long data){
+    Node *temp = (Node *) malloc(sizeof(Node));
     temp->key = key;
     temp->data = data;
     temp->left = NULL; temp->right=NULL;temp->parent = NULL;
     temp->isBlack = 0; //All new node are red
+    temp->isLeftChild = 0;
     return temp;
-};
+}
 
 void add(Node **redBlack, unsigned long key, unsigned long data){
     Node *temp = newNode(key, data);
     if(!(*redBlack)){
         temp->isBlack = 1;
         *redBlack = temp;
-        return;
-    }
-    if( (*redBlack)->key <  key){//go to right
-        add(&(*redBlack)->right, key, data);
+    }else if( (*redBlack)->key <  key){//go to right
+        add(&((*redBlack)->right), key, data);
         (*redBlack)->right->parent = (*redBlack);
         (*redBlack)->right->isBlack = 0; //All new node are red
+        temp = (*redBlack)->right;
     }
 
     else if( (*redBlack)->key >  key){//go to left
-        add(&(*redBlack)->left, key, data);
+        add(&((*redBlack)->left), key, data);
         (*redBlack)->left->parent = (*redBlack);
-        (*redBlack)->right->isBlack = 0; //All new node are red
+        (*redBlack)->left->isBlack = 0; //All new node are red
+        (*redBlack)->left->isLeftChild = 1;//we are the left child
+        temp = (*redBlack)->left;
     }
-
 
     else{
         printf("ERROR! - Invalid key");
     }
+    //we need check if any rule has been breaked
+    checkColor(&temp);
     return;
 }
+
+void checkColor(Node **node){
+    if(!(*node)) return; //node is null
+    if((*node)->parent == NULL) return; //root dont have violations
+    if( !(*node)->isBlack && !(*node)->parent->isBlack){ //two consecutive reds
+        correctTree(&(*node));
+    }
+    checkColor(&(*node)->parent); //check if we dont cause a violation after fix a violation
+}
+
+/*
+int isAuntBlack(Node *node){
+    if(!node) return;
+    //if our parent is left child, our aunt is the parent of my parent on right
+    if(node->parent->isLeftChild){
+        if (node->parent->parent->right == NULL){
+        //  ----DAD-----
+        //  ------GRANDFATHER-----
+        //  -----------------AUNT-------- (BECAUSE THEY ARE THE OTHER SON OF MY GRANDFATHER)
+            return 1; // All null nodes are black
+        }
+        return node->parent->parent->right->isBlack; //If i have an aunt
+    }
+    //if i'm the right child, my aunt will be the left son of my grandfather
+    if( node->parent->parent->left == NULL)
+        return 1;
+    return node->parent->parent->left->isBlack;
+}
+*/
+
+void correctTree(Node **node){
+    if((*node)->parent->parent->isLeftChild){
+        //aunt is parent->parent->right
+        if((*node)->parent->parent->right == NULL || (*node)->parent->parent->right->isBlack){
+            printf("_");
+            //rotateNode(&(*node));
+        }
+        if((*node)->parent->parent->right != NULL){ //ColorFlip
+            (*node)->parent->parent->right->isBlack = 1; //The childs are blacks and parent are red
+        }
+        (*node)->parent->parent->isBlack = 0; //parent red
+        (*node)->parent->isBlack = 1;
+        return;
+    }
+    //aunt is parent->parent->left
+    if((*node)->parent->parent->left == NULL || (*node)->parent->parent->left->isBlack){
+        printf("_");
+        //rotateNode(&(*node));
+    }
+    if((*node)->parent->parent->left != NULL){ //ColorFlip
+        (*node)->parent->parent->left->isBlack = 1; //The childs are blacks and parent are red
+    }
+    (*node)->parent->parent->isBlack = 0; //parent red
+    (*node)->parent->isBlack = 1;
+    return;
+}
+
+
 
 
 //Root is always BLACK
@@ -64,7 +127,22 @@ void add(Node **redBlack, unsigned long key, unsigned long data){
 
 //after colorFlip:
 //Red with two blaco children
-void main(){
-    Node *RedBlack = NULL;
+void traversal(Node *RedBlack){
+    if(!RedBlack) return;
+    printf("%d\n", RedBlack->key);
+    traversal(RedBlack->left);
+    traversal(RedBlack->right);
+}
 
+
+void main(){
+    srand(time(NULL));
+    Node *RedBlack = NULL;
+    int i;
+    add(&RedBlack, 12, 10000);
+    add(&RedBlack, 8, 10000);
+    //add(&RedBlack, 21, 10000);
+    add(&RedBlack, 6, 10000);
+    //printf("%d", RedBlack->right->key);
+    traversal(RedBlack);
 }
